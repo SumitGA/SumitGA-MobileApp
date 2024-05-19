a74ac1e3e7a0fc8f3b1058a09f3c704ad4931c58
SumitGA <sumit@intersect.org.au>
2024-05-19 15:42:22 +1000

    Add react-native paper styling dependencies to react native app

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {
   Text,
 } from 'react-native';
 
+
 const App: React.ElementType = () => {
 
   return (
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { PaperProvider } from 'react-native-paper';
+
+export default function Main() {
+    return(
+        <PaperProvider>
+            <App />
+        </PaperProvider>
+    )
+}
 
 AppRegistry.registerComponent(appName, () => App);
